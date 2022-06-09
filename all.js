4ae2e24e3b7fdbc4a972562fa8fa53a68ea82f91
redtail <hongwei0522@gmail.com>
2022-06-09 13:31:45 +0800

    notification

---
+++ all.js
@@ -0,0 +1,13 @@
+var notifyConfig = {
+    body: '\\ ^o^ /', // 設定內容
+    icon: '/images/favicon.ico', // 設定 icon
+  };
+  
+  if (Notification.permission === 'default' || Notification.permission === 'undefined') {
+    Notification.requestPermission(function(permission) {
+      if (permission === 'granted') {
+        // 使用者同意授權
+        var notification = new Notification('Hi there!', notifyConfig); // 建立通知
+      }
+    });
+  }
